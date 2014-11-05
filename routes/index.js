b2ceb3d0f65c109598b07b20b653ab92f108795b
aw3s0me <aw3s0me@rocketmail.com>
2014-11-05 20:21:14 +0400

    Adding users

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,36 @@ router.get('/helloworld', function(req, res){
 	res.render('helloworld', { title: 'Hello, World!'});
 });
 
+router.get('/newuser', function(req, res) {
+	res.render('newuser', { title: 'Add New User'});
+});
+
+router.post('/adduser', function(req, res) {
+	var db = req.db;
+
+	var userName = req.body.username;
+	var userMail = req.body.useremail;
+
+	var collection = db.get('usercollection');
+
+	collection.insert({
+		"username": userName,
+		"email": userMail
+	}, function(err, doc) {
+		if (err) {
+			//if failure return error
+			res.send("There was a problem adding info to db");
+		}
+		else {
+			//if it worked set the header
+			res.location('userlist');
+			// Forward to userlist
+			res.redirect('userlist');
+		}
+	});
+
+});
+
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
     var db = req.db;
@@ -21,5 +51,7 @@ router.get('/userlist', function(req, res) {
         });
     });
 });
+
+
 
 module.exports = router;
