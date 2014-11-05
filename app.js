e1633adb040483f75846a7d117401198e8e24042
aw3s0me <aw3s0me@rocketmail.com>
2014-11-05 20:03:44 +0400

    Added mongodb files + modified app.js + user list + data

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
+var routes = require('./routes/index'); //combination of models and controllers
 var users = require('./routes/users');
 
-var app = express();
+var app = express(); //instantiate express
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,9 +24,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //static objects from public dir, like coming from top level
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
+// Make our db accessible to our router
+
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
